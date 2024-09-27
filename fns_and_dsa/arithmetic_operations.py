176af04c9abcf98e2d34692a26e58e4241ac8129
root <allanleboo292@gmail.com>
2024-09-26 20:50:11 -0700

    check for error

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,8 +7,8 @@ def perform_operation(num1, num2, operation):
         return num1 * num2
     elif operation == "divide":
         if num2 == 0:
-            return "Error : division by zero"
+            return "Division by zero is undefined"
         else:
             return num / num2
     else:
-        return "Error: invalid operation"
+        return "Invalid operation"
