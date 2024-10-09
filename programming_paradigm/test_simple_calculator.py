71e29a2ee1cdc35f60758ff3a67a6e72da19e8b6
root <allanleboo292@gmail.com>
2024-10-09 00:41:51 -0700

    Added test_division

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -26,7 +26,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(0, 5), 0)
         self.assertEqual(self.calc.multiply(-5, -3), 15)
 
-    def test_divide(self):
+    def test_division(self):
         
         self.assertEqual(self.calc.divide(6, 3), 2)
         self.assertEqual(self.calc.divide(-6, 3), -2)
